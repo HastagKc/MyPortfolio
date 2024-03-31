3883c535266e676273b83c717945e42bfccfc714
HastagKc <chaudharykshittiz950@gmail.com>
2024-03-31 21:56:09 +0545

    styling upto skills container complete

--- myportfolio/static/js/app.js
+++ myportfolio/static/js/app.js
@@ -12,3 +12,21 @@ backButton.addEventListener('click', function(event) {
         behavior: 'smooth' // Smooth scrolling animation
     });
 });
+
+// responsive hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+
